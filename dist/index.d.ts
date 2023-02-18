e5e501583e793339a61f190c61a9167cd54a6ed2
ArturoMellado <alberto.mellado@globant.com>
2023-02-18 16:19:46 -0600

    feat: Semantic versioning

---
+++ dist/index.d.ts
@@ -0,0 +1,4 @@
+export { Button } from "./stories/Button";
+export { CustomLabel } from "./components/label/CustomLabel";
+export { Header } from "./stories/Header";
+export { Page } from "./stories/Page";
