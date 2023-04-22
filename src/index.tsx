a362ce99b5b84420fd9aee6fe1129ac007a8b1f3
bardock-ozaru <bardock2210@gmail.com>
2023-04-21 22:33:12 -0600

    fix: TextInput component was modified

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,1 @@
-export { Button } from "./stories/Button";
-export { Header } from "./stories/Header";
-export { Page } from "./stories/Page";
 export { TextInput } from "./stories/components/form/text-field/TextInput";
