061f7b09832041eed55c58f5994871c3c4a4a194
bardock-ozaru <bardock2210@gmail.com>
2023-04-23 01:38:30 -0600

    fix: Select component was modified

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.TextInput = exports.Select = void 0;
-var Select_1 = require("@/components/form/select/Select");
+var Select_1 = require("./stories/components/form/select/Select");
 Object.defineProperty(exports, "Select", { enumerable: true, get: function () { return Select_1.Select; } });
-var TextInput_1 = require("@/components/form/text-field/TextInput");
+var TextInput_1 = require("./stories/components/form/text-field/TextInput");
 Object.defineProperty(exports, "TextInput", { enumerable: true, get: function () { return TextInput_1.TextInput; } });
--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,2 @@
-export { Select } from "@/components/form/select/Select";
-export { TextInput } from "@/components/form/text-field/TextInput";
+export { Select } from "./stories/components/form/select/Select";
+export { TextInput } from "./stories/components/form/text-field/TextInput";
