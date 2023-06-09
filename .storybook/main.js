50e3ea72fd8a6ef75f8832e54b7c9ab58226e172
bardock-ozaru <bardock2210@gmail.com>
2023-06-08 19:36:14 -0600

    fix: alias path configuration was added

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,33 @@
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", {
-    name: "@storybook/addon-postcss",
-    options: {
-      postcssLoaderOptions: {
-        implementation: require("postcss")
-      }
-    }
-  }, "@storybook/addon-mdx-gfm"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: require("postcss"),
+        },
+      },
+    },
+    "@storybook/addon-mdx-gfm",
+  ],
+  webpackFinal: async () => {
+    config.resolve.modules = [path.resolve(__dirname, ".."), "node_modules"];
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@/assets": path.resolve(__dirname, "../src/stories/assets"),
+      "@/components": path.resolve(__dirname, "../src/stories/components"),
+      "@/types": path.resolve(__dirname, "../src/stories/types"),
+    };
+  },
   framework: {
     name: "@storybook/react-webpack5",
-    options: {}
+    options: {},
   },
   docs: {
-    autodocs: true
-  }
+    autodocs: true,
+  },
 };
