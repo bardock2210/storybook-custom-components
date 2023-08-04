fd57ee9f5df7fd37586d69c87c31db8b1beca0c2
bardock-ozaru <bardock2210@gmail.com>
2023-08-03 22:30:54 -0600

    fix: the panel component was exported

--- src/index.tsx
+++ src/index.tsx
@@ -15,6 +15,7 @@ export { default as MobileMenu } from "@/components/display/MobileMenu";
 export { default as Select } from "@/components/form/elements/Select";
 export { default as TextInput } from "@/components/form/elements/TextField";
 export { default as ProfileCard } from "@/components/display/ProfileCard";
+export { default as Panel } from "@/components/container/Panel";
 
 // Hooks
 export { useDynamicForm } from "@/hooks/index";
